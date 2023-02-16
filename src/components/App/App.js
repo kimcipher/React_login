6c80fdc4da660078d2190a05e9285810b7915996
kimcipher <kimutaij192@gmail.com>
2023-02-16 16:38:53 +0300

    Login

---
+++ src/components/App/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import './App.css';
+import Dashboard from '../Dashboard/Dashboard';
+import Login from '../Login/Login';
+import Preferences from '../Preferences/Preferences';
+import userToken from './useToken';
+
+
+function App() {
+  const { token, setToken } = userToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
